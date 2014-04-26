c7da645e1b8d16ccd238cc198b58118fad5e1b1e
molon <dudl@qq.com>
2014-04-27 03:17:51 +0800

    Initial Commit

---
+++ MLPictureBroswer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MLPictureBroswer
+//
+//  Created by molon on 4/27/14.
+//  Copyright (c) 2014 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MLPictureBroswerTests/MLPictureBroswerTests.m
@@ -0,0 +1,34 @@
+//
+//  MLPictureBroswerTests.m
+//  MLPictureBroswerTests
+//
+//  Created by molon on 4/27/14.
+//  Copyright (c) 2014 molon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MLPictureBroswerTests : XCTestCase
+
+@end
+
+@implementation MLPictureBroswerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
